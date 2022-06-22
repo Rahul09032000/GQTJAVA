d8aaa115ee76b3766dc19c6eff8f80a385ac9843
Rahul <rahulmahendrakar09@gmail.com>
2022-06-22 23:27:41 +0530

    New Codes added

---
+++ ApnaCollege_Java/src/ApnaCollegeHomeworks/L4HW1.java
@@ -0,0 +1,23 @@
+package ApnaCollegeHomeworks;
+
+import java.util.Scanner;
+
+public class L4HW1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println("All Even numbers till "+ n +" are ");
+		
+		for (int i=1; i<=n;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
+
